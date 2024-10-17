51e1953632e2ae8b94bd46d07f3fd9d3fa6a7636
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-10-17 15:57:59 -0400

    Add logic to display number in the UI

--- script.js
+++ script.js
@@ -6,6 +6,37 @@ multiply
 divide
 */
 
+const textDisplay = document.querySelector(".display-text");
+const container = document.querySelector(".container");
+
+let currentNumber = ""
+let previousNumber = "";
+
+container.addEventListener("click" , (e) => {
+    e.preventDefault();
+
+    const userSelection = e.target.value
+
+    switch(userSelection) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9": 
+            if(currentNumber.length < 20) {
+                currentNumber += userSelection;
+                textDisplay.textContent = currentNumber;
+            }
+           
+        break;
+
+    }
+})
 
 function add () {
 
